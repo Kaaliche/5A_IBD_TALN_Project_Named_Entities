1cd42413b5a27ab0a5710f3de3eb81fe82825c6c
adrian <petricevicadrian@gmail.com>
2018-10-26 11:12:09 +0200

    [FIX] Import

--- ner_parser/parser/englishNerParser.py
+++ ner_parser/parser/englishNerParser.py
@@ -1,4 +1,4 @@
-from ner_parser.parser import Parser
+from ner_parser.parser.parser import Parser
 from named_entites.document import Document
 
 class EnglishNerParser(Parser):
@@ -7,7 +7,7 @@ class EnglishNerParser(Parser):
         documents = []
 
         # 1. Split the text in documents using string '-DOCSTART- -X- O O' and loop over it
-        
+
         # 2. Slit lines and loop over
         # 3. Make vectors of tokens and labels (colunn 4) and at the '\n\n' make a sentence
         # 4. Create a Document object
