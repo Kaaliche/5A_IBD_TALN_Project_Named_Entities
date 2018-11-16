320d81ecb147e2e9ed21eb33531159c95f190a52
Pattachou <chou.95230@live.fr>
2018-11-16 13:46:21 +0100

    [UPD] add list of sentences in englishNerParser

--- ner/parser/englishNerParser.py
+++ ner/parser/englishNerParser.py
@@ -9,16 +9,29 @@ class EnglishNerParser(Parser):
         for doc in content.split('-DOCSTART- -X- O O'):
             if doc == '':
                 continue
-            words, tag = [], []
+            words, tag, sentences = [], [], []
+            i = 0
+            start_cursor = 0
             for line in doc.splitlines():
                 if line.split():
                     words.append(line.split()[0])
                     tag.append(line.split()[3])
+                    i += 1
+                elif i != 0:
+                    sentence = ""
+                    for j in range(start_cursor, i):
+                        if start_cursor == i:
+                            sentence += words[j]
+                        else:
+                            sentence += words[j] + " "
+                        j += 1
+                    start_cursor = i
+                    sentences.append(sentence)
 
         # 1. Split the text in documents using string '-DOCSTART- -X- O O' and loop over it
         # 2. Split lines and loop over
         # 3. Make vectors of tokens and labels (colunn 4) and at the '\n\n' make a sentence
         # 4. Create a Document object
-        documents.append(Document.create_from_vectors(words, sentences, tag))
+            documents.append(Document.create_from_vectors(words, sentences, tag))
 
         return documents
