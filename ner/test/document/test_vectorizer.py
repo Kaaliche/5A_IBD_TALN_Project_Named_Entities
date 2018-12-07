180afe19d14f4fe12ecfcd13bea54d26ec1c3ef1
Pattachou <chou.95230@live.fr>
2018-12-07 16:19:41 +0100

    [EDT] Test vectorize

--- ner/test/document/test_vectorizer.py
+++ ner/test/document/test_vectorizer.py
@@ -9,13 +9,30 @@ from ner.parser import EnglishNerParser
 class TestVectorizer(TestCase):
 
 
-    def test_read(self):
+    def test_count(self):
         filename = os.path.join(DATA_DIR, 'files', 'test.txt')
         parse = EnglishNerParser().read_file(filename)
         path_embeding = os.path.join(DATA_DIR, 'files', 'glove.6B.50d.txt')
+        vector = Vectorizer(path_embeding)
 
-        word, pos, shape = Vectorizer(path_embeding).encode_features(parse)
+        word, pos, shape = vector.encode_features(parse)
+        label = vector.encode_annotations(parse)
 
-        self.assertEqual(len(word), 2)
+        self.assertEqual(max(len(word), len(shape), len(pos)), 2)
+        self.assertEqual(len(label), 2)
 
-        Vectorizer(path_embeding).encode_annotations(parse)
+
+    def tes_first_terme(self):
+        filename = os.path.join(DATA_DIR, 'files', 'test.txt')
+        parse = EnglishNerParser().read_file(filename)
+        path_embeding = os.path.join(DATA_DIR, 'files', 'glove.6B.50d.txt')
+        vector = Vectorizer(path_embeding)
+
+        word, pos, shape = vector.encode_features(parse)
+        label = vector.encode_annotations(parse)
+
+        self.assertEqual(word[0][0], 644)
+        self.assertEqual(pos[0][0], 38)
+        self.assertEqual(shape[0][0], 3)
+        self.assertEqual(label[0][0], 644)
+
