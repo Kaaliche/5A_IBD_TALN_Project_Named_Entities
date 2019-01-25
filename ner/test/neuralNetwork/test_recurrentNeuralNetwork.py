d4ee253eec3a48b9bef12b338ce34fa68a3a6a0a
adrian <petricevicadrian@gmail.com>
2019-01-25 07:18:13 +0100

    [UPD] RNN Test OK"

--- ner/test/neuralNetwork/test_recurrentNeuralNetwork.py
+++ ner/test/neuralNetwork/test_recurrentNeuralNetwork.py
@@ -1,15 +1,19 @@
 import unittest
+import os
 
 from ner.document import Vectorizer
 from ner.document import Document
 from ner.neuralNetwork.recurrentNeuralNetwork import RecurrentNeuralNetwork
 from ner.parser import EnglishNerParser
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 file = "C:\\Users\\Adrian\\test.txt"
 
 class TestRecurrentNeuralNetwork(unittest.TestCase):
 
-    def setUp(self):
+    '''def setUp(self):
         #with open(file, "r", encoding="utf-8") as fn:
             #content = fn.read()
             #doc = Document().create_from_text(file)
@@ -20,10 +24,17 @@ class TestRecurrentNeuralNetwork(unittest.TestCase):
                 'shape': (2,2)
             }
             out_shape = (2,2)
-            self.model = RecurrentNeuralNetwork.build_sequence(word_embeddings.word_embedding, input_shape, out_shape)
+            self.model = RecurrentNeuralNetwork.build_sequence(word_embeddings.word_embedding, input_shape, out_shape)'''
 
     def test_build_sequence(self):
-        self.assertIsInstance(self.model, RecurrentNeuralNetwork)
+        word_embeddings = Vectorizer("C:\\Users\\Adrian\\glove.6B.50d.txt")
+        input_shape = {
+            'pos': (10, 10),
+            'shape': (2, 2)
+        }
+        out_shape = 2
+        self.assertIsInstance(RecurrentNeuralNetwork.build_sequence(word_embeddings.word_embedding, input_shape, out_shape),
+                              RecurrentNeuralNetwork)
 
 if __name__ == '__main__':
     unittest.main()
